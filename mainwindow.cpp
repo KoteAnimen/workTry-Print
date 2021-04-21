e662960ecda7db722434758bd303af6230d21577
KoteAnimen <sverh.il@yandex.ru>
2021-04-21 15:00:14 +0300

    Теперь коды печатаются правильно в GS1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include "QDebug"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -52,12 +52,42 @@ void MainWindow::Print(QString str)
 
 void MainWindow::on_pushButton_clicked()
 {
-    Print(QString("^XA")
-          + QString("^FO 200,100")
-          + QString("^FB400,2,10,C,0")
-          + QString("^ASN,10,10")
-          + QString("^FD")
-          + QString(ui->lineEdit->text())
-          + QString("^FS")
-          + QString("^XZ"));
+    if(path != "")
+    {
+        Print("^XA"
+              "^FO 360,40"  //смещение текста от левого верхнего края
+              "^FB400,2,10,C,0" //ширина, количество строк, пробелы между строками, выравнивание текста, отступ для второй или последующей строки
+              "^ASN,10,10" //шрифт S и размер букв 10 на 10 точек
+              "^BXN,5,200,,,,_"
+              "^FD_1" + QString(array[(int)ui->numberDataMatrix->value()]) + "^FS"//сам текст
+              "^XZ");
+    }
+    else
+    {
+        QMessageBox box;
+        box.setWindowTitle("Ошибка");
+        box.setText("Не загружен файл с DataMatrix");
+        box.exec();
+    }
+
+}
+
+void MainWindow::on_Download_clicked()
+{
+    int i = 0;
+    path = QFileDialog::getOpenFileName(this, QString::fromUtf8("Открыть файл"), QDir::currentPath(),"File (*.csv)");
+    QFile file(path);
+    if(file.open(QFile::ReadOnly))
+    {
+       QTextStream in(&file);
+       while(!in.atEnd())
+       {
+           fromFileInfo = in.readLine();
+           array[i] = fromFileInfo;
+           i++;
+       }
+    }
+    qDebug()<<array[1];
+
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,9 @@
 #include <QAbstractSocket>
 #include <QNetworkProxy>
 #include <QMessageBox>
+#include <QFileDialog>
+#include <QFile>
+#include <QTextStream>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -16,12 +19,18 @@ class MainWindow : public QMainWindow
 
 public:
     MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();    
+    ~MainWindow();
+    QString fromFileInfo = "";
+
+    QString array[5000];
+    QString path = "";
 
     void Print(QString);
 
 private slots:
     void on_pushButton_clicked();
+
+    void on_Download_clicked();
 
 private:
     Ui::MainWindow *ui;
